7a1f9d41e38fd666856da5c9715483a7cfa5617f
famabarham <sfama@ept.sn>
2023-07-10 09:26:54 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn HOW TO DEPLOY REACT ON VERCEL
         </a>
       </header>
     </div>
